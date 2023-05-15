263d29d3e81e9093dddb257a3b6b5a311fa432ed
naiara.martinez <naiara.martinez@docplanner.com>
2023-05-15 18:14:33 +0200

    Logout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,14 +3,20 @@
     <a href="#">Home</a>
     <a href="#">Add Restaurant</a>
     <a href="#">Update Restaurant</a>
-    <a href="#">Logout</a>
+    <a href="#" v-on:click="logout">Logout</a>
   </div>
 </template>
 
 <script>
   export default {
     // eslint-disable-next-line vue/multi-word-component-names
-    name: 'Header'
+    name: 'Header',
+    methods:{
+      logout() {
+        localStorage.clear();
+        this.$router.push({name: 'LoginPage'});
+      }
+    }
   }
 </script>
 
